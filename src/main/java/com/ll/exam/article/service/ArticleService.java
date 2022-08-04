0a70b47045b66a623755039ecd9c463550bb03c8
Shin-sangwon <dnjsml10@gmail.com>
2022-08-04 11:48:56 +0900

    write 테스트

--- src/main/java/com/ll/exam/article/service/ArticleService.java
+++ src/main/java/com/ll/exam/article/service/ArticleService.java
@@ -23,4 +23,10 @@ public class ArticleService {
     public long getArticlesCount() {
         return articleRepository.getArticlesCount();
     }
+
+    public long write(String title, String body, boolean isBlind) {
+
+        return articleRepository.write(title, body, isBlind);
+
+    }
 }
